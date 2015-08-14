7cc035c99dfb8f044ead52a4ccaae255fedb0c62
mwweinberg <mw@michaelweinberg.org>
2015-08-14 16:31:03 -0400

    this is the first working version of the scraper

---
+++ scrape.py
@@ -0,0 +1,28 @@
+# based on https://first-web-scraper.readthedocs.org/en/latest/
+
+import csv
+import requests
+from BeautifulSoup import BeautifulSoup
+
+url = 'http://bleacherreport.com/articles/2546185-fantasy-football-2015-qb-rankings-projections-and-top-team-names'
+response = requests.get(url)
+html = response.content
+
+
+
+
+soup = BeautifulSoup(html)
+# this is building off of the line <div class="bleacher-table">
+table = soup.find('div', attrs={'class': 'bleacher-table'})
+
+list_of_rows = []
+for row in table.findAll('tr'):
+	list_of_cells = []
+	for cell in row.findAll('td'):
+		text = cell.text
+		list_of_cells.append(text)
+	list_of_rows.append(list_of_cells)
+
+outfile = open("./players.csv", "wb")
+writer = csv.writer(outfile)
+writer.writerows(list_of_rows)
